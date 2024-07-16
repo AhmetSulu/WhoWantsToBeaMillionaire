1c90cbdb89dfb3db1fd3e2477ecaa3547c1063a8
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-07-16 22:58:56 +0300

    Yorum Ekledim

--- KimMilyoner/Program.cs
+++ KimMilyoner/Program.cs
@@ -1,9 +1,14 @@
 ﻿int correctAnswerCount = 0;
 
+
+// İlk soruyu soruyoruz
+
 Console.WriteLine("Kızınca tukuren hayvan hangisidir?");
 Console.WriteLine("A) Lama");
 Console.WriteLine("B) Deve");
 Console.WriteLine("Cevabın");
+
+// Cevabı alıyoruz
 
 string answer1 = Console.ReadLine().ToLower();
 
@@ -17,11 +22,13 @@ else
 {
     Console.WriteLine("Yanlış!");
 }
-
+// İkinci soruyu soruyoruz
 Console.WriteLine("Dünyaya en yakın gezegen hangisidir?");
 Console.WriteLine("A) Venüs");
 Console.WriteLine("B) Mars");
 Console.WriteLine("Cevabın");
+
+//Cevabı alıyoruz
 
 string answer2 = Console.ReadLine().ToLower();
 
@@ -35,13 +42,17 @@ else
 {
     Console.WriteLine("Yanlış!");
 }
-
+// Burda ilk 2 soruya doğru cevabı verirse 3.soruya gecmeden ödül kazandınız ekranı geliyor
 if (correctAnswerCount == 2 )
 {
     Console.WriteLine("Tebrikler! 3 sorudan 2 soruya doğru cevabı verdin 1 Milyon TL ödülü kazandın!");
 }
 else
 {
+    // Egerki ilk 2 soruyu yanlış yaptıysan 3.soruyu soruyor ve toplamda 2 doğru cevap alındıysa ödül kazandınız ekranı geliyor.
+    // Veya tek soruya doğru cevabı verdiyse ödülü kazanamadınız ekrani geliyor.
+
+
     Console.WriteLine("5 * 2 + 8 / 2 - 2 işleminin sonucu kaçtır ?");
     Console.WriteLine("A) 7");
     Console.WriteLine("B) 12");
